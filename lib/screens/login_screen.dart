7c219d2a3cf554fc803f4a7e17a7c836ebb0ac74
Danny <live626@gmail.com>
2021-07-27 21:19:37 -0400

    contenedor blanco

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,7 +6,27 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AuthBackground()
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+
+              SizedBox(height: 250),
+
+              CardContainer(
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Text('Login', style: Theme.of(context).textTheme.headline4),
+                    SizedBox(height: 30),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        )
+      )
     );
   }
 }
